17512c7db23dbffe32f021bde921a1afbfbb0a2b
Darren Wee <darren.wee@u.nus.edu>
2019-04-22 19:51:20 +0800

    Add run on optimal value in optimize script

--- assignment-4/optimize.py
+++ assignment-4/optimize.py
@@ -3,30 +3,33 @@ from numpy import arange
 
 
 def optimize_round_robin(processes: List[Process], lower: int = 1, upper: int = 10, step_size: int = 1):
+    results = dict()
     with open('rr-op.txt', 'w') as f:
         # write header
         f.write('quantum avg.wait\n')
 
         for Q in range(lower, upper + step_size, step_size):
             sched, avg_wait = RR_scheduling(deepcopy(processes), time_quantum=Q)
+            results[Q] = avg_wait
             line = '%s %.5f\n' % (Q, avg_wait)
             f.write(line)
-            print(line.strip())
-    return
+            # print(line.strip())
+    return min(results, key=results.get)
 
 
 def optimize_sjf(processes: List[Process], lower: float = 0.0, upper: float = 1.0, step_size: float = 0.05):
+    results = dict()
     with open('sjf-op.txt', 'w') as f:
         # write header
         f.write('alpha avg.wait\n')
 
         for alpha in arange(lower, upper + step_size, step_size):
             sched, avg_wait = SJF_scheduling(deepcopy(processes), alpha=alpha)
+            results[alpha] = avg_wait
             line = '%.3f %.5f\n' % (alpha, avg_wait)
             f.write(line)
-            print(line.strip())
-
-    return
+            # print(line.strip())
+    return min(results, key=results.get)
 
 
 if __name__ == '__main__':
@@ -39,7 +42,11 @@ if __name__ == '__main__':
         print(process)
 
     print("simulating RR ----")
-    optimize_round_robin(process_list, lower=1, upper=15, step_size=1)
+    Q = optimize_round_robin(process_list, lower=1, upper=15, step_size=1)
+    rr_sched, rr_wait = RR_scheduling(process_list, time_quantum=Q)
+    write_output('rr-optimal-%s.txt' % Q, rr_sched, rr_wait)
 
     print("simulating SJF ----")
-    optimize_sjf(process_list, lower=0.0, upper=1.0, step_size=0.02)
+    alpha = optimize_sjf(process_list, lower=0.0, upper=1.0, step_size=0.02)
+    sjf_sched, sjf_wait = SJF_scheduling(process_list, alpha=alpha)
+    write_output('sjf-optimal-%s.txt' % alpha, sjf_sched, sjf_wait)
